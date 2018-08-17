3faf0654c2ab90dd666e6abfe4abe18f874e3042
haad <haad@haja.io>
2018-08-17 09:02:43 +0300

    Make EntryIO functions async and use await

--- src/entry-io.js
+++ src/entry-io.js
@@ -6,12 +6,12 @@ const Entry = require('./entry')
 
 class EntryIO {
   // Fetch log graphs in parallel
-  static fetchParallel (ipfs, hashes, length, exclude = [], concurrency, timeout, onProgressCallback) {
+  static async fetchParallel (ipfs, hashes, length, exclude = [], concurrency, timeout, onProgressCallback) {
     const fetchOne = (hash) => EntryIO.fetchAll(ipfs, hash, length, exclude, timeout, onProgressCallback)
     const concatArrays = (arr1, arr2) => arr1.concat(arr2)
     const flatten = (arr) => arr.reduce(concatArrays, [])
-    return pMap(hashes, fetchOne, { concurrency: Math.max(concurrency || hashes.length, 1) })
-      .then(flatten) // Flatten the results
+    const result = await pMap(hashes, fetchOne, { concurrency: Math.max(concurrency || hashes.length, 1) })
+    return flatten(result)
   }
 
   /**
@@ -26,7 +26,7 @@ class EntryIO {
    * @param {function(hash, entry, parent, depth)} onProgressCallback
    * @returns {Promise<Array<Entry>>}
    */
-  static fetchAll (ipfs, hashes, amount, exclude = [], timeout = null, onProgressCallback) {
+  static async fetchAll (ipfs, hashes, amount, exclude = [], timeout = null, onProgressCallback) {
     let result = []
     let cache = {}
     let loadingQueue = Array.isArray(hashes)
@@ -52,7 +52,7 @@ class EntryIO {
         return Promise.resolve()
       }
 
-      return new Promise((resolve, reject) => {
+      return new Promise(async (resolve, reject) => {
         // Resolve the promise after a timeout (if given) in order to
         // not get stuck loading a block that is unreachable
         const timer = timeout 
@@ -75,17 +75,18 @@ class EntryIO {
         }
 
         // Load the entry
-        Entry.fromMultihash(ipfs, hash)
-          .then(addToResults)
-          .then(resolve)
-          .catch(err => {
-            resolve()
-          })
+        try {
+          const entry = await Entry.fromMultihash(ipfs, hash)
+          addToResults(entry)
+          resolve()
+        } catch (e) {
+          reject(e)
+        }
       })
     }
 
-    return pWhilst(shouldFetchMore, fetchEntry)
-      .then(() => result)
+    await pWhilst(shouldFetchMore, fetchEntry)
+    return result
   }
 }
 
